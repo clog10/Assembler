3985d80bbb34fb2498b716e0a9721c4e5ec895e1
Carlos Loaeza <48633809+clog10@users.noreply.github.com>
2020-03-13 23:33:07 -0600

    Add files via upload

---
+++ LOOP.asm
@@ -0,0 +1,44 @@
+;@utor Clog10
+
+.286
+PILA SEGMENT STACK
+    DB 32 DUP('STACK_')
+PILA ENDS
+
+DATOS SEGMENT 
+DATOS ENDS
+
+CODIGO SEGMENT 'CODE'
+    
+    MAIN PROC FAR
+    
+    ASSUME CS:CODIGO, DS:DATOS, SS:PILA
+    
+     PUSH DS
+     PUSH 0
+     MOV AX, DATOS
+     MOV DS, AX
+     
+     MOV CX,5
+     LINEA1:
+     PUSH CX
+     MOV AH, 02H;Posicionar el Cursor
+     MOV DH, 00H;Fila 5
+     MOV DL, 28H;Columna 40 Dec
+     INT 10H
+     MOV CX,1
+     LINEA2:
+     
+     MOV AH, 02H ;Salida del caracter
+     MOV DL, 2AH ;Codigo ascii del caracter (*) 42 Dec
+     INT 21H
+     LOOP LINEA2
+     POP CX
+    LOOP LINEA1
+   
+    
+       RET
+     MAIN ENDP
+ CODIGO ENDS
+ END MAIN
+     
