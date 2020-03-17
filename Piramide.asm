2052229e74853bd9ac430f787ea767d35df7dd6c
Carlos Loaeza <48633809+clog10@users.noreply.github.com>
2020-03-16 21:29:02 -0600

    Piramide hacia arriba (Completo)
    Piramide hacia arriba (Completo)

--- Piramide.asm
+++ Piramide.asm
@@ -1,93 +1,56 @@
  ;@utor Clog10
+    
+    CURSOR MACRO VX,VY
+        MOV AH, 02H;Posicionar el Cursor
+        MOV BH, 0
+        MOV DH, VY;Fila
+        MOV DL, VX;Columna 
+             
+        INT 10H
+      ENDM
 
-.286
-PILA SEGMENT STACK
-    DB 32 DUP('STACK_')
-PILA ENDS
-
-DATOS SEGMENT 
+STOCK SEGMENT STACK
+    DB 8 DUP ('stack_')
+STOCK ENDS
+DATAS SEGMENT
+      COLX DB 28H
+      FILY DB 0
+      ACUM DB 1
+DATAS ENDS
+CODE SEGMENT 'CODE'
+    ASSUME SS:STOCK,DS:DATAS,CS:CODE
     
-DATOS ENDS
-
-CODIGO SEGMENT 'CODE'
-    
-    MAIN PROC FAR
-    
-    ASSUME CS:CODIGO, DS:DATOS, SS:PILA
-    
-     PUSH DS
-     PUSH 0
-     MOV AX, DATOS
-     MOV DS, AX
-     
-     MOV CX, 3   ;Cantidad de * a imprimir
-     
-;-----LINEA UNO---------    
-     MOV AH, 02H;Posicionar el Cursor
-     MOV DH, 00H;Fila 0
-     MOV DL, 28H;Columna 40 Dec
-     INT 10H
-     
-     MOV AH, 02H;Salida del caracter
-     MOV DL, 2AH;Codigo ascii del caracter (*) 42 Dec
-     INT 21H
-     
-;------LINEA DOS-------     
-     MOV AH, 02H ;Posicionar el Cursor
-     MOV DH, 01H ;Fila 1
-     MOV DL, 27H ;Columna 39 Dec
-     INT 10H
-      
-    LINEA2:
-     MOV AH, 02H ;Salida del caracter
-     MOV DL, 2AH ;Codigo ascii del caracter (*) 42 Dec
-     INT 21H
-     LOOP LINEA2
-     
-;------LINEA TRES------  
-     MOV CX, 5    ;Cantidad de * a imprimir
+    PRINC PROC FAR
         
-     MOV AH, 02H ;Posicionar el Cursor
-     MOV DH, 02H  ;Fila 2
-     MOV DL, 26H  ;Columna 38 Dec
-     INT 10H
-     
-     LINEA3:
-     MOV AH, 02H ;Salida del caracter
-     MOV DL, 2AH ;Codigo ascii del caracter (*) 42 Dec
-     INT 21H
-     LOOP LINEA3
-     
-;------LINEA CUATRO------  
-     MOV CX, 7  ;Cantidad de * a imprimir
-        
-     MOV AH, 02H ;Posicionar el Cursor
-     MOV DH, 03H ;Fila 3
-     MOV DL, 25H ;Columna 37 Dec
-     INT 10H
-     
-     LINEA4:
-     MOV AH, 02H ;Salida del caracter
-     MOV DL, 2AH ;Codigo ascii del caracter (*) 42 Dec
-     INT 21H
-     LOOP LINEA4
-      
-;------LINEA CINCO------  
-     MOV CX, 9  ;Cantidad de * a imprimir
-        
-     MOV AH, 02H ;Posicionar el Cursor
-     MOV DH, 04H ;Fila 4
-     MOV DL, 24H  ;Columna 36 Dec
-     INT 10H 
-     
-     LINEA5:
-     MOV AH, 02H ;Salida del caracter
-     MOV DL, 2AH  ;Codigo ascii del caracter (*) 42 Dec
-     INT 21H
-     LOOP LINEA5
+        PUSH DS
+        PUSH 0
+        MOV AX, DATAS
+        MOV DS,AX
 
-     RET
-     MAIN ENDP
- CODIGO ENDS
- END MAIN
-     
+        CURSOR COLX,FILY
+        
+        MOV CX,5 ;ciclo 1, 20 veces
+        
+        CICLO_1:;INICIO CICLO 1
+            PUSH CX ;guardar valor del ciclo 1
+            MOV CL,ACUM ; ciclo 2
+            CICLO_2:;INICIO CICLO 2
+                MOV AH,2H
+                MOV DL,2AH
+                INT 21H 
+            LOOP CICLO_2 ;FIN CICLO 2
+            
+            ADD ACUM,2
+            INC FILY  
+            DEC COLX  
+            CURSOR COLX,FILY
+            POP CX  ;regresar valor ciclo 1
+        LOOP CICLO_1     ;FIN CICLO 1
+                    
+        RET
+    PRINC ENDP
+
+CODE ENDS
+END PRINC
+
+                   
